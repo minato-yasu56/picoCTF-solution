64527f4a924541479037a7fd51ad6cb9ef4b3f6f
minato anzai <minato39524.sc@gmail.com>
2026-01-28 19:46:48 +0900

    section2/MOD26 説明を追加

--- The Beginner's Guide to the picoGym/section2/mod26.py
+++ The Beginner's Guide to the picoGym/section2/mod26.py
@@ -4,8 +4,15 @@ M = ""
 for c in C:
     code = ord(c)
     if c.islower():
+        # a ~ z : 0 ~ 25
+        # c の場合 (99[c] - 97[a] - 13) % 26 = 15
+        # 15 + 97 = 112 => p
         code = (code - ord('a') - shift) % 26 + ord('a')
     elif c.isupper():
+        # A ~ Z : 0 ~ 25
         code = (code - ord('A') - shift) % 26 + ord('A')
     M += chr(code)
 print(M)
+print(ord('a'))
+print(ord('A'))
+print((ord('c') - ord('a') - 13) % 26)
